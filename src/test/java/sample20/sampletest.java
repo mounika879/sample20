b7d4e3b051f47cb9015532b7efe14312d00462c8
mounika <kodatimounika@gmail.com>
2019-06-17 19:54:07 +0530

    this is msg

---
+++ src/test/java/sample20/sampletest.java
@@ -0,0 +1,14 @@
+package sample20;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sampletest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
